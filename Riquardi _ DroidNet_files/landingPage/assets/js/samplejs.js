a323a8e19f2da7b8b860fce56b7d71ae5d1878f8
Tushar Pandey <tusharp1.45@gmail.com>
2021-03-22 02:59:27 +0530

    landingPageUploaded
    testing

---
+++ Riquardi _ DroidNet_files/landingPage/assets/js/samplejs.js
@@ -0,0 +1,22 @@
+//sampleJS
+function clickedWaiting() {
+  window.open("https://thetusharpandey.github.io","_self");
+};
+$('#linkIcon').click(function(){
+  $('#riquardi').css({
+    'animation-duration':'3s',
+    'animation-name': 'logoResize',
+    'animation-timing-function': 'cubic-bezier(.43,-0.04,.3,1.2)'
+  });
+  $('.content').css({
+    'animation-duration':'0.7s',
+    'animation-name': 'opacityDown',
+    'opacity':'0'
+  });
+  $('#navigation').css({
+    'animation-duration':'0.7s',
+    'animation-name': 'opacityDown',
+    'opacity':'0'
+  });
+  setTimeout(clickedWaiting,3000);
+});
